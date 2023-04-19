b9aad745d1cb8d9140900df9dc582696d4d4d789
muzafferugur <av.muzafferugur@gmail.com>
2023-04-19 16:51:14 +0300

    19.04.2023

--- src/test/java/day02_driverMethodlari/C07_manageMethodları.java
+++ src/test/java/day02_driverMethodlari/C07_manageMethodları.java
@@ -1,7 +1,10 @@
 package day02_driverMethodlari;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class C07_manageMethodları {
     public static void main(String[] args) {
-
+    WebDriver driver=new ChromeDriver();
     }
 }
