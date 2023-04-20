0b362a1c89426c0e78a5ef326c3b89608dea8abf
muzafferugur <av.muzafferugur@gmail.com>
2023-04-21 01:04:09 +0300

    21.04.2023

---
+++ src/test/java/day04_xpath_cssLocator/C03_CssSelector.java
@@ -0,0 +1,58 @@
+package day04_xpath_cssLocator;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C03_CssSelector {
+    public static void main(String[] args) throws InterruptedException {
+        /*
+1 ) Bir class oluşturun : Locators_css
+2)Main method oluşturun ve aşağıdaki görevi tamamlayın.
+a. Verilen web sayfasına gidin. http://a.testaddressbook.com/sign_in
+b. Locate email textbox
+c. Locate password textbox ve
+d. Locate signin button
+e. Asagidaki kullanıcı adını ve şifreyi girin ve sign in düğmesini
+tıklayın
+i. Username : testtechproed@gmail.com
+ii. Password : Test1234!
+NOT: cssSelector kullanarak elementleri locate ediniz.
+*/
+
+
+        System.setProperty("webdriver.chrome.driver","src/drivers/chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        //Verilen web sayfasına gidin. http://a.testaddressbook.com/sign_in
+        driver.get("http://a.testaddressbook.com/sign_in");
+
+        //b. Locate email textbox
+        WebElement email= driver.findElement(By.cssSelector("#session_email"));
+
+      //c. Locate password textbox ve
+        WebElement password= driver.findElement(By.cssSelector("#session_password"));
+
+        //   d. Locate signin button
+        WebElement signin= driver.findElement(By.cssSelector("input[name='commit']"));
+        Thread.sleep(3000);
+
+        ////   e. Asagidaki kullanıcı adını ve şifreyi girin ve sign in düğmesini tıklayın
+        //i. Username : testtechproed@gmail.com
+        //ii. Password : Test1234!
+
+        email.sendKeys("testtechproed@gmail.com");
+        password.sendKeys("Test1234!");
+        signin.click();
+
+        Thread.sleep(3000);
+        driver.close();
+
+
+    }
+}
